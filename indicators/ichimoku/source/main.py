680b44e22a808fe6660824b171d6d8d9ceb5e516
omid <o.h.e.1380@gmail.com>
2022-01-06 19:55:24 +0330

    Update main.py
    move displacement from "calculating" to "plotting"

--- indicators/ichimoku/source/main.py
+++ indicators/ichimoku/source/main.py
@@ -34,8 +34,7 @@ def donchian(high_list, low_list):
 def calculate_ichimoku(data, #is a list of Candle objects
                        conversion_periods=9,
                        base_periods=26,
-                       lagging_span_periods=52,
-                       displacement=26
+                       lagging_span_periods=52
                        ):
     open_prices = [candle.o for candle in data]
     high_prices = [candle.h for candle in data]
@@ -44,9 +43,9 @@ def calculate_ichimoku(data, #is a list of Candle objects
 
     result = []
 
-    for i in range(len(data) + displacement):
+    for i in range(len(data)):
         #conversion:
-        if(len(data) > i and i >= conversion_periods):
+        if(i >= conversion_periods):
             high_list = high_prices[i - conversion_periods + 1: i + 1]
             low_list = low_prices[i - conversion_periods + 1 : i + 1]
             conversion = donchian(high_list, low_list)
@@ -54,72 +53,77 @@ def calculate_ichimoku(data, #is a list of Candle objects
             conversion = None
         
         #base:
-        if(len(data) > i and i >= base_periods):
-            high_list = high_prices[i - base_periods + 1 : i + 1]
-            low_list = low_prices[i - base_periods + 1 : i + 1]
+        if(i >= base_periods):
+            high_list = high_prices[i + 1 - base_periods : i + 1]
+            low_list = low_prices[i + 1 - base_periods : i + 1]
             base = donchian(high_list, low_list)
         else:
             base = None
             
         #lead1:
-        try:
-            temp_conversion = result[-displacement].conversion
-            temp_base = result[-displacement].base
-            lead1 = (temp_conversion + temp_base) / 2
-        except:
+        if(conversion and base):
+            lead1 = conversion + base
+            lead1 /= 2
+        else:
             lead1 = None
 
         #lead2:
-        if(i >= lagging_span_periods + displacement):
-            high_list = high_prices[i - lagging_span_periods - displacement + 1 : i - displacement + 1]
-            low_list = low_prices[i - lagging_span_periods - displacement + 1 : i - displacement + 1]
+        if(i >= lagging_span_periods):
+            high_list = high_prices[i + 1 - lagging_span_periods : i + 1]
+            low_list = low_prices[i + 1 - lagging_span_periods : i + 1]
             lead2 = donchian(high_list, low_list)
         else:
             lead2 = None
 
         #lagging_span:
-        if(i + displacement <= len(data)):
-            lagging_span = close_prices[i + displacement - 1]
-        else:
-            lagging_span = None
+        lagging_span = close_prices[i]
 
         new_item = Ichimoku(conversion, base, lead1, lead2, lagging_span)
         result.append(new_item)
 
     return result
 
+
 def plot_ichimoku(data,
+                  displacement = 26,
                   conversion_color = '#2962FF',
                   base_color = '#B71C1C',
                   lead1_color = '#A5D6A7',
                   lead2_color = '#EF9A9A',
                   lagging_span_color = '#43A047',
                   ):
+    displacement -= 1
+    D = [None for i in range(displacement)]
     
-    conversion_line = [i.conversion for i in data]
+    conversion_line = array(D + [i.conversion for i in data] + D)
     plt.plot(conversion_line, color=conversion_color)
 
-    base_line = [i.base for i in data]
+    base_line = array(D + [i.base for i in data] + D)
     plt.plot(base_line, color = base_color)
 
-    lead_line1 = [i.lead1 for i in data]
+    lead_line1 = array(D + D + [i.lead1 for i in data])
     plt.plot(lead_line1, color = lead1_color)
 
-    lead_line2 = [i.lead2 for i in data]
+    lead_line2 = array(D + D + [i.lead2 for i in data])
     plt.plot(lead_line2, color = lead2_color)
 
-    lagging_span = [i.lagging_span for i in data]
+    lagging_span = array([i.lagging_span for i in data] + D + D)
     plt.plot(lagging_span, color = lagging_span_color)
 
     
-    x = range(len(data))
+    x = range(2 * displacement + len(data))
+    
     for i in data:
         if(not i.lead1 or not i.lead2):
             i.lead1 = 0
             i.lead2 = 0
-    lead1 = [i.lead1 for i in data]
-    lead2 = [i.lead2 for i in data]
-    plt.fill_between(x, lead1, lead2, color=lead2_color + '50', where=[i.lead1 > i.lead2 for i in data])
-    plt.fill_between(x, lead1, lead2, color=lead1_color +'50', where=[i.lead1 < i.lead2 for i in data])
+            
+    D = [0 for i in range(displacement)]
+    lead1 = array(D + D + [i.lead1 for i in data])
+    lead2 = array(D + D + [i.lead2 for i in data])
+
+    W = [False for i in range(displacement)]
+    plt.fill_between(x, lead1, lead2, color=lead1_color+'50', where= W + W + [i.lead1 > i.lead2 for i in data])
+    plt.fill_between(x, lead1, lead2, color=lead2_color+'50', where= W + W + [i.lead1 < i.lead2 for i in data])
     
     plt.show()
